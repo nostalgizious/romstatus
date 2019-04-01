488e20cb4372f9583799bd76eaf2173468587269
Ferdinand Karl Opsahl Schnell <fersch@Ferdinands-MacBook-Air-640.local>
2019-04-02 00:51:21 +0200

    Lagde fil for å sette opp database til lagring av data

---
+++ opprettDatabase.py
@@ -0,0 +1,34 @@
+import psycopg2
+from psycopg2 import sql
+from config import config
+from oppdateringsHastighet import romListen
+
+""" Connect to the PostgreSQL database server """
+conn = None
+try:
+    # read connection parameters
+    params = config()
+    #print(params)
+    # connect to the PostgreSQL server
+    print('Connecting to the PostgreSQL database...')
+    conn = psycopg2.connect(**params)
+    # create a cursor
+    cur = conn.cursor()
+    # execute a statement
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+    # display the PostgreSQL database server version
+    db_version = cur.fetchone()
+    print(db_version)
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    if conn is not None:
+        for romNummer in romListen():
+            rom = 'rom'+str(romNummer)
+            cur.execute("SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_name=%s)", ('rom',))
+            if bool(cur.rowcount) == True:
+                 cur.execute(sql.SQL('''CREATE TABLE {}(ID INTEGER PRIMARY KEY, datetime TEXT, co2ppm INTEGER, tempc FLOAT, humidity FLOAT, irsensor INTEGER)''').format(sql.Identifier(rom)))
+                 print('created table '+rom)
+            else:
+                continue
